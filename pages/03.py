121d6d34acc77df94f66ba49049214cfc3a739a3
kwagdongan <142889476+kwagdongan@users.noreply.github.com>
2026-06-13 20:24:37 +0900

    Update 03.py

--- pages/03.py
+++ pages/03.py
@@ -13,7 +13,7 @@ df['total_reviews'] = pd.to_numeric(df['total_reviews'], errors='coerce')
 df['positive_percentual'] = pd.to_numeric(df['positive_percentual'], errors='coerce')
 
 # 리뷰 수가 10개 이상인 게임만 대상으로 산점도 출력 (신뢰도 확보)
-filtered_df = df[df['total_reviews'] >= 10].copy()
+filtered_df = df[(df['total_reviews'] > 0) & (df['positive_percentual'] > 0)].copy()
 
 # 2. 산점도 출력
 top_reviews_df = filtered_df.nlargest(500, 'total_reviews')
