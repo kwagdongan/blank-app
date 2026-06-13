e8e9887e766bf57b3fa6b1c104681e7f58340305
kwagdongan <142889476+kwagdongan@users.noreply.github.com>
2026-06-13 21:11:49 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ if 'df' not in st.session_state:
     st.session_state['df'] = load_data()
 
 # 1. 데이터 로드 및 정제 후
-df['genres_list'] = df['genre'].apply(clean_genres)
+df['genres_list'] = df['genres'].apply()
 
 # [추가] 분석 시작 전 'Indie' 장르를 모든 행의 리스트에서 제거
 df['genres_list'] = df['genres_list'].apply(lambda x: [g for g in x if g != 'Indie'])
