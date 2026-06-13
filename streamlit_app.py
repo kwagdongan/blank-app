75f4da679eee0bcc7818c623c2087a556bf843e6
kwagdongan <142889476+kwagdongan@users.noreply.github.com>
2026-06-13 19:54:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="Steam 분석", layout="wide")
+
+st.title("제목 테스트 중")
+st.write("사이드바")
