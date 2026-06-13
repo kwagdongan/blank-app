66fd3f9a0c616a97ec2ea78fcc0a375530fee2d8
kwagdongan <142889476+kwagdongan@users.noreply.github.com>
2026-06-13 20:12:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,11 @@ st.set_page_config(page_title="Steam 분석", layout="wide")
 
 st.title("제목 테스트 중")
 st.write("사이드바")
+
+@st.cache_data
+def load_data():
+    return pd.read_csv('datas.csv', encoding='cp949')
+
+# 세션에 데이터가 없으면 로드해서 저장
+if 'df' not in st.session_state:
+    st.session_state['df'] = load_data()
