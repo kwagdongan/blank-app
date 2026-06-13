b704d6c0ecccf291e82e89ffd1255d603828ea85
kwagdongan <142889476+kwagdongan@users.noreply.github.com>
2026-06-13 20:12:49 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.write("사이드바")
 
 @st.cache_data
 def load_data():
-    return pd.read_csv('datas.csv', encoding='cp949')
+    return pd.read_csv('datas.csv', encoding='utf-8-sig')
 
 # 세션에 데이터가 없으면 로드해서 저장
 if 'df' not in st.session_state:
